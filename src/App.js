506738748be5a3235847feb492b0e0ead04de594
James Miller <james.rex.miller.4@gmail.com>
2022-03-02 13:02:19 -0700

    Integrate Results component, setup useEffect with fetchFirstPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import TextField from './components/TextField';
+// import axios from "axios";
+import { fetchFirstPage } from './apiCalls/commons';
+import Results from './components/Results';
 
 function App() {
+  const [results, setResults] = useState("")
+  const [nextPageToken, setNextPageToken] = useState("")
+
+  useEffect(() => {
+    // get initial results for displaying
+    const results = fetchFirstPage()
+
+    if (results.status === 200) {
+      setResults(results.data.pokemon)
+      setNextPageToken(results.data.nextPage)
+    }
+
+  }, [])
+
   return (
     <main className="App">
       <TextField />
+      <Results />
     </main>
   );
 }
