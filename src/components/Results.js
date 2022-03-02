fcffcee3668e3c91e529bbffd4035a21a569356c
James Miller <james.rex.miller.4@gmail.com>
2022-03-02 14:36:59 -0700

    Refactor rResults to display pokemon id

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Results({ loading, results, error }) {
 
-    const displayedResults = Array.isArray(results) ? results.map((result) => {
-        return (<li id={result.id} key={result.id}>
+    const displayedResults = Array.isArray(results) ? results.map((result, idx) => {
+        return (<li id={result.id} key={idx}>
             <h2>Name: {result.name}</h2>
+            <p>Pokemon ID: {result.id}</p>
             <p>Classification: {result.classfication}</p>
         </li>)
     }) : results
