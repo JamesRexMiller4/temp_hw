aba147d35b2876fa442f5612e4cdd96528fdf80f
James Miller <james.rex.miller.4@gmail.com>
2022-03-02 13:26:19 -0700

    Manipulate results into displayedResults const in Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 export default function Results({ results }) {
 
+    const displayedResults = results.map((result) => {
+        return (<li id={result.id} key={result.id}>
+            <h2>Name: {result.name}</h2>
+            <p>Classification: {result.classfication}</p>
+        </li>)
+    })
+
     return (
         <section>
-            { results }
+            <ul>
+                { displayedResults }
+            </ul>
         </section>
     )
 }
